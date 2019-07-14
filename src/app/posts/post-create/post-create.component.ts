94293673a4882dd142e8ef38fad21333c80fe9a0
girwarkishor <girwar.kishor@gmail.com>
2019-07-14 13:35:02 +0530

    Added Form Method One in post create component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-post-create',
@@ -11,10 +12,13 @@ export class PostCreateComponent{
     enteredTitle = '';
     @Output() postCreated = new EventEmitter<Post>();
 
-    onAddPost(){
+    onAddPost(form: NgForm){
+        if(form.invalid){
+            return;
+        }
         const post: Post = {
-            title: this.enteredTitle,
-            content: this.enteredContent
+            title: form.value.title,
+            content: form.value.content
         };
         this.postCreated.emit(post); 
     }
