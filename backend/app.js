0175fa1398a2e00da6ecdb847ca61d6572a4759e
girwarkishor <girwar.kishor@gmail.com>
2019-07-20 20:53:14 +0530

    Added Cross Origin Resource Sharing

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader(
+        "Access-Control-Allow-Header",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS"
+    );
+    next();
+});
+
 app.use("/api/posts", (req, res, next)=>{
     const posts=[
         {
