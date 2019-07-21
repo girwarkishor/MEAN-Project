1667f1ec022538b772ae6071b13685e1f216ff41
girwarkishor <girwar.kishor@gmail.com>
2019-07-21 08:58:59 +0530

    Added post without mongobd cloud connection

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Post = require("./models/post");
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -19,8 +21,13 @@ app.use((req, res, next) => {
     next();
 });
 
+// ayskeiRphOGJBs7M
+
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
     console.log(post);
     res.status(201).json({
         message: 'Post added successfully'
