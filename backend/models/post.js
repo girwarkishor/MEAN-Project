72eb094166fe8e13f4ad1dede64807cb5d4d6363
girwarkishor <girwar.kishor@gmail.com>
2019-07-21 08:02:25 +0530

    Added Mongoose

---
+++ backend/models/post.js
@@ -0,0 +1,11 @@
+const mongoose =  require('mongoose');
+
+//its just a blue print of data
+const postSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    content: { type: String, required: true }
+});
+
+//now we have to use it so we need it to export
+
+module.exports = mongoose.model('Post', postSchema);
