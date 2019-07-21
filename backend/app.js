cc7fd13c6bd53718a829cde999816048f98f9303
girwarkishor <girwar.kishor@gmail.com>
2019-07-21 13:35:20 +0530

    Storing Data in a Database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
 const Post = require("./models/post");
 
 const app = express();
+
+//Password: ayskeiRphOGJBs7M
+mongoose.connect("mongodb+srv://girwar:ayskeiRphOGJBs7M@cluster0-0k6ma.mongodb.net/node-angular?retryWrites=true&w=majority")
+.then(() => {
+    console.log("Connected to database!");
+})
+.catch(() => {
+    console.log("Connection failed!");
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -21,13 +32,13 @@ app.use((req, res, next) => {
     next();
 });
 
-// ayskeiRphOGJBs7M
 
 app.post("/api/posts", (req, res, next) => {
     const post = new Post({
         title: req.body.title,
         content: req.body.content
     });
+    post.save();
     console.log(post);
     res.status(201).json({
         message: 'Post added successfully'
