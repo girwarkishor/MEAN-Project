f69f512b14ec920847537ec1debff9547cddee8f
girwarkishor <girwar.kishor@gmail.com>
2019-07-24 21:32:26 +0530

    Transforming Response Data

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'}) //it just provided one instance in this app no need to add in app.model.ts
 export class PostsService{
@@ -11,9 +12,20 @@ export class PostsService{
     constructor(private http: HttpClient){}
 
     getPosts(){
-       this.http.get<{message: string, posts: Post[]}>("http://localhost:3000/api/posts")
-       .subscribe((postData) => {
-           this.posts = postData.posts;
+       this.http.get<{message: string, posts: any}>(
+           "http://localhost:3000/api/posts"
+        )
+        .pipe(map((postData) => {
+            return postData.posts.map(post => {
+                return {
+                    title: post.title,
+                    content: post.content,
+                    id: post._id
+                };
+            });
+        }))
+       .subscribe((transformedPosts) => {
+           this.posts = transformedPosts;
            this.postsUpdated.next([...this.posts]);
        });
         //return [...this.posts]; //coping the original post | spread operator
