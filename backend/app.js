ee6933b2fee8b0aed356d075096f3ca1e6da2def
girwarkishor <girwar.kishor@gmail.com>
2019-07-28 08:09:21 +0530

    Reorganise the Router at backend

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
 
-const Post = require("./models/post");
+const postsRoutes = require("./routes/posts");
 
 const app = express();
  
@@ -32,68 +32,6 @@ app.use((req, res, next) => {
     next();
 });
 
-
-app.post("/api/posts", (req, res, next) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-    post.save().then(createdPost => {
-        res.status(201).json({
-            message: 'Post added successfully',
-            postId: createdPost._id
-        });
-    });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-        if(post){
-            res.status(200).json(post);
-        } else {
-            res.status(404).json({message: 'Post not found!'});
-        }
-    });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    Post.updateOne({_id: req.params.id}, post).then(result => {
-        res.status(200).json({message: "Update successful!"});
-    });
-});
-
-app.get("/api/posts", (req, res, next)=>{
-    // const posts=[
-    //     {
-    //         id: "ljasdflkj123",
-    //         title: "First server-side post",
-    //         content: "This is coming from the server"
-    //     },
-    //     {
-    //         id: "oiuweqour123",
-    //         title: "Second server-side post",
-    //         content: "This is coming from the server!"
-    //     }
-    // ]; dummy content
-
-    Post.find().then(documents => {
-        res.status(200).json({
-            message: "Posts fetched successfully!",
-            posts: documents
-        });
-    }); // postSchema function
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-    Post.deleteOne({_id: req.params.id}).then(result => {
-        console.log(result);
-        res.status(200).json({message: "Post deleted!"});
-    })
-});
+app.use("/api/posts",postsRoutes);
 
 module.exports = app;
