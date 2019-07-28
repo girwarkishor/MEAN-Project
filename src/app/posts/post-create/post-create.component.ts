3c9f8ab7b95ee3a9b36eb725fc274ee799380338
girwarkishor <girwar.kishor@gmail.com>
2019-07-28 21:55:31 +0530

    Added image preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -61,8 +61,11 @@ export class PostCreateComponent implements OnInit {
         const file = (event.target as HTMLInputElement).files[0];
         this.form.patchValue({image: file});
         this.form.get('image').updateValueAndValidity();
-        console.log(file);
-        console.log(this.form);
+        const reader = new FileReader();
+        reader.onload = () => {
+            this.imagePreview = reader.result;
+        };
+        reader.readAsDataURL(file);
   }
 
   onSavePost() {
