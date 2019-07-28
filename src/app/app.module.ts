181e04942c720c2d66b8c82b5d7be7357e8287b2
girwarkishor <girwar.kishor@gmail.com>
2019-07-28 20:52:19 +0530

    Converting the Form from a Template Driven to Reactive approach

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, MatProgressSpinnerModule } from '@angular/material';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -21,7 +21,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,57 +1,75 @@
-import {Component, OnInit} from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { PostsService } from '../posts.service';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Post } from '../post.model';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+
+import { PostsService } from "../posts.service";
+import { Post } from "../post.model";
 
 @Component({
-    selector: 'app-post-create',
-    templateUrl: './post-create.component.html',
-    styleUrls: ['./post-create.component.css']
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
 })
-export class PostCreateComponent implements OnInit{
-    enteredContent = '';
-    enteredTitle = '';
-    post: Post;
-    isLoading = false;
-    private mode = 'create';
-    private postId: string;
+export class PostCreateComponent implements OnInit {
+  enteredTitle = "";
+  enteredContent = "";
+  post: Post;
+  isLoading = false;
+  form: FormGroup;
+  imagePreview: string;
+  private mode = "create";
+  private postId: string;
 
-    constructor(public postsService: PostsService, public route: ActivatedRoute){}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
-    ngOnInit(){
-        this.route.paramMap.subscribe((paramMap: ParamMap) => {
-            if(paramMap.has('postId')){
-                this.mode = 'edit';
-                this.postId = paramMap.get('postId');
-                this.isLoading = true;
-                this.postsService.getPost(this.postId).subscribe(postData => {
-                    this.isLoading = false;
-                    this.post = {id: postData._id, title: postData.title, content: postData.content};
-                });
-            } // postId name as written in router
-            else{
-                this.mode = 'create';
-                this.postId = null;
-            }
-        });
-    }
-
-    onSavePost(form: NgForm){
-        if(form.invalid){
-            return;
-        }
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, { validators: [Validators.required] })
+    });
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
         this.isLoading = true;
-        if(this.mode === 'create'){
-            this.postsService.addPost(form.value.title, form.value.content);
-        } else {
-            this.postsService.updatePost(
-                this.postId,
-                form.value.title,
-                form.value.content
-            );
-        }
-        
-        form.resetForm();
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.isLoading = false;
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content
+          };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content
+          });
+        });
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
+      return;
     }
+    this.isLoading = true;
+    if (this.mode === "create") {
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        this.form.value.title,
+        this.form.value.content
+      );
+    }
+    this.form.reset();
+  }
 }
