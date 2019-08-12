541ca13b655759c2d7f8eb0837da031e79c611d6
girwarkishor <girwar.kishor@gmail.com>
2019-08-12 10:56:39 +0530

    Returning Error Messages on the Server

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -64,6 +64,8 @@ export class PostListComponent implements OnInit, OnDestroy {
     this.isLoading = true;
     this.postsService.deletePost(postId).subscribe(() => {
       this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    }, () => {
+      this.isLoading = false;
     });
   }
 
