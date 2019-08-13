38062d4366420243d89dfc109f2230475657bb87
girwarkishor <girwar.kishor@gmail.com>
2019-08-13 06:41:04 +0530

    Fixing an Update Bug

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -41,7 +41,7 @@ exports.createPost = (req, res, next) => {
       { _id: req.params.id, creator: req.userData.userId },
       post
     ).then(result => {
-      if (result.nModified > 0) {
+      if (result.n > 0) {
         res.status(200).json({ message: "Update successful!" });
       } else {
         res.status(401).json({ message: "Not authorized!" });
